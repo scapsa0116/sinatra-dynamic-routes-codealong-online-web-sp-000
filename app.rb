cf4944fd3ae27c2120a1da8b352d2bee1bf255e3
Stela Capsa <stela.capsa@gmail.com>
2020-03-28 18:43:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ class App < Sinatra::Base
 
   # This is a sample static route.
   get '/hello' do
-    "Hello World!"
+    erb :index
+   
   end
 
   # This is a sample dynamic route.
@@ -14,5 +15,17 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do 
+    @user_name = params[:name]
+   erb :goodbye
+  end 
+  
+  get '/multiply/:num1/:num2' do 
+  @user_name.select do |user|
+    user.id == params[:id]
+  end.first 
+end 
+  
+  
 
 end
